3462eabddd89aaf8b239ef0de4ea2da05f90b95e
Juri_Bürkle <rastopka@gmx.degit config --global user.email rastopka@gmx.de>
2025-05-13 20:08:00 +0200

    class Ticket

--- src/main/java/com/example/booking/model/Ticket.java
+++ src/main/java/com/example/booking/model/Ticket.java
@@ -1,10 +1,69 @@
 package com.example.booking.model;
 
+import java.util.Objects;
+
 public class Ticket {
     private String ticketId;          // номер билета
     private Passenger passenger;
     private Flight flight;
     private TicketStatus status;      // статус: ACTIVE/CANCELLED
+    private static int ticketCounter = 0;
 
-    // getters, setters, equals, hashCode, toString
+    public Ticket(Flight flight, Passenger passenger) {
+        this.flight = flight;
+        this.passenger = passenger;
+        this.status = TicketStatus.ACTIVE;
+        this.ticketId = String.valueOf(ticketCounter);
+        ticketCounter ++;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public void setStatus(TicketStatus status) {
+        this.status = status;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public TicketStatus getStatus() {
+        return status;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return Objects.equals(ticketId, ticket.ticketId) && Objects.equals(passenger, ticket.passenger) && Objects.equals(flight, ticket.flight) && status == ticket.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ticketId, passenger, flight, status);
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "ticketId='" + ticketId + '\'' +
+                ", passenger=" + passenger +
+                ", flight=" + flight +
+                ", status=" + status +
+                '}';
+    }
 }
