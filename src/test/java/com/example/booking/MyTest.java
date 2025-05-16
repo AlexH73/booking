9b879aafbb8e7755a6714709024a485eaa99c4eb
Alex_H <ewebotah@gmail.com>
2025-05-16 11:41:10 +0200

    update
    Пересобрал pom.xml: исправил ошибки с версиями, добавил модуль для юнет-тестов и исправил мелкие ошибки. Обновление проекта MAVEN командой "mvn clean install"

---
+++ src/test/java/com/example/booking/MyTest.java
@@ -0,0 +1,12 @@
+package com.example.booking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class MyTest {
+    @Test
+    void testExample() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
